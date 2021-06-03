7a936b8948ac86c73385ca66e0921b78283d68e2
K M Emon Ahmed <kmemon.ah@gmail.com>
2021-06-03 13:07:55 +0600

    Component tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainComponent from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainComponent />
     </div>
   );
 }
