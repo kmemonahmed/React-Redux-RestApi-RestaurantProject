7804673ff35837388236ed56e4f6e4bed99a77d7
K M Emon Ahmed <kmemon.ah@gmail.com>
2021-06-10 12:03:18 +0600

    project moc complete

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -5,9 +5,24 @@ class Home extends Component {
     render() {
         document.title="Emon Restaurant";
         return (
-            <div>
-                <Loading />
-            </div>
+                <div className="row align-items-center justify-content-center" style={{backgroundImage:"url(/assets/images/3cake.jpg)",
+                 width: '100vw',
+                 height: '100vh',
+                 backgroundPosition: "center",
+                 backgroundRepeat: "no-repeat",
+                 backgroundSize: "cover",
+                 opacity: "0.5"
+                 }}>
+                        <h1 className="col font-weight-bold" style={{
+                            backgroundColor: 'white',
+                            color:'black',
+                            maxWidth: 'fit-content'
+                        }}>
+                            Welcome to Emon Restaurant
+                         </h1>
+
+                </div>
+
         )
     }
 }
